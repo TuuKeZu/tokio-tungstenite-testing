a97a194158b09d39dc0f5d96a9b5ea10023fbf33
TuuKeZu <moilanentuukka5@gmail.com>
2022-10-28 11:11:38 +0300

    implemented a 'broadcast' functionality for lobbies

--- src/bin/rewrite.rs
+++ src/bin/rewrite.rs
@@ -1,15 +1,16 @@
-use futures::stream::StreamExt;
+use futures::stream::{SplitSink, StreamExt};
 use futures::SinkExt;
 use hyper::service::{make_service_fn, service_fn};
+use hyper::upgrade::Upgraded;
 use hyper::{Body, Request, Response, Server};
-use hyper_tungstenite::tungstenite::Message;
-use hyper_tungstenite::HyperWebsocket;
+use hyper_tungstenite::tungstenite::{Message, WebSocket};
+use hyper_tungstenite::{HyperWebsocket, WebSocketStream};
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::sync::mpsc::{self, Sender};
-use tokio::sync::RwLock;
+use tokio::sync::{Mutex, RwLock};
 use uuid::Uuid;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -17,35 +18,61 @@ type LobbyMessage = Message;
 
 struct Lobby {
     id: Uuid,
+    connections: HashMap<usize, Arc<Mutex<SplitSink<WebSocketStream<Upgraded>, Message>>>>,
 }
 
 impl Lobby {
     fn new() -> Lobby {
-        Lobby { id: Uuid::new_v4() }
+        Lobby {
+            id: Uuid::new_v4(),
+            connections: HashMap::new(),
+        }
     }
-    pub fn handle_message(&self, msg: Message, id: usize) {
+    pub async fn handle_message(&mut self, msg: Message, id: usize) {
         println!("Connection {id} received: {msg}");
+        self.broadcast(msg).await;
+    }
+
+    pub async fn broadcast(&mut self, msg: Message) {
+        for (id, connection) in self.connections.iter_mut() {
+            connection.lock().await.send(msg.clone()).await;
+        }
     }
 }
 
 async fn handle_messages(
     websocket: HyperWebsocket,
-    mapping: Arc<RwLock<HashMap<usize, Arc<Lobby>>>>,
+    mapping: Arc<RwLock<HashMap<usize, Arc<Mutex<Lobby>>>>>,
     id: usize,
 ) -> Result<(), Error> {
-    let mut websocket = websocket.await?;
-    while let Some(message) = websocket.next().await {
+    let websocket = websocket.await?;
+
+    let (ws_write, mut ws_read) = websocket.split();
+
+    let mut mapping_lock = mapping.write().await;
+    let lobby = mapping_lock.get_mut(&id).unwrap();
+    lobby
+        .lock()
+        .await
+        .connections
+        .insert(id, Arc::new(Mutex::new(ws_write)));
+
+    std::mem::drop(mapping_lock);
+
+    while let Some(message) = ws_read.next().await {
         let message = message?;
         let text = message.to_text()?;
         if let Some(lobby) = mapping.read().await.get(&id) {
+            let mut l = lobby.lock().await;
             println!(
                 "[lobby {} with {} users] Connection {id} received: {text}",
-                lobby.id,
+                l.id,
                 Arc::strong_count(lobby)
             );
             // websocket.send(message).await.unwrap();
             // tx.send(message).await?;
-            lobby.handle_message(message, id);
+            l.handle_message(message, id).await;
+            std::mem::drop(l);
         } else {
             unreachable!()
         }
@@ -59,7 +86,7 @@ async fn handle_messages(
 
 async fn handle_connection(
     mut request: Request<Body>,
-    mapping: Arc<RwLock<HashMap<usize, Arc<Lobby>>>>,
+    mapping: Arc<RwLock<HashMap<usize, Arc<Mutex<Lobby>>>>>,
 ) -> Result<Response<Body>, Error> {
     dbg!(&request);
 
@@ -69,7 +96,7 @@ async fn handle_connection(
     if let Some(lobby) = mapping_lock.iter().next().map(|(_, l)| Arc::clone(l)) {
         mapping_lock.insert(id, lobby);
     } else {
-        mapping_lock.insert(id, Arc::new(Lobby::new()));
+        mapping_lock.insert(id, Arc::new(Mutex::new(Lobby::new())));
     };
 
     std::mem::drop(mapping_lock);
